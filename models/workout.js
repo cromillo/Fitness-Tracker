eece47d26f85a331568b657e286ad51505edb9d4
Claire Romillo <c_romillo@yahoo.com>
2019-11-07 09:37:07 -0800

    updated api routes and schema

--- models/workout.js
+++ models/workout.js
@@ -2,18 +2,31 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema(
-  },
-  {
-    toJSON: {
-      // include any virtual properties when data is requested
-      virtuals: true
+const workoutSchema = new Schema({
+  day: new Date(Date.now()),
+  exercises: [
+    {
+      type: String,
+      name: String,
+      duration: Integer,
+      distance: Integer,
+      weight: Integer,
+      reps: Integer,
+      sets: Integer,
     }
+  ]
+})
+
+
+  toJSON: {
+    // include any virtual properties when data is requested
+    virtuals: true
   }
-);
+
+
 
 // adds a dynamically-created property to schema
-workoutSchema.virtual("totalDuration").get(function() {
+workoutSchema.virtual("totalDuration").get(function () {
   // "reduce" array of exercises down to just the sum of their durations
   return this.exercises.reduce((total, exercise) => {
     return total + exercise.duration;
--- routes/api.js
+++ routes/api.js
@@ -3,3 +3,42 @@ const Workout = require("../models/workout.js");
 
 
 module.exports = router;
+
+router.get("/",function(req,res){
+  res.sendFile(path.join(__dirname,"../public/index.html"))
+})
+
+app.get("/api/workouts", ({ body }, res) => {
+  db.Workout.create(body)
+    .then(({ _id }) => db.Workout.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.post("/api/workouts", ({ body }, res) => {
+  db.Workout.create(body)
+    .then(({ _id }) => db.Workout.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+
+  app.get("/all", (req, res) => {
+    db.Workout.find({}, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(data)
+        res.json(data);
+    }
+});
+});
+
